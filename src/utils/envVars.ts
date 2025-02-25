cec83bdb24fdf28c00be21a812c2d11e5a746843
D3athgr1p <86771928+D3athGr1p@users.noreply.github.com>
2025-02-25 17:56:53 +0530

    fixing chain bug

--- src/utils/envVars.ts
+++ src/utils/envVars.ts
@@ -9,6 +9,7 @@ export const RPC_URL                    = process.env.REACT_APP_RPC_URL ||  (`ht
 
 // public
 export const NETWORK_NAME               = IS_MAINNET ? 'Mainnet' : TESTNET_LAUNCHPAD_NAME;
+export const CUSTOM_NETWORK             = Number(process.env.REACT_APP_CUSTOM_NETWORK);
 export const TICKER_NAME                = IS_MAINNET ? 'ETH' : `${NETWORK_NAME}ETH`;
 export const ETHERSCAN_URL              = IS_MAINNET ? 'https://etherscan.io/tx' : `https://${TESTNET_LAUNCHPAD_NAME.toLowerCase()}.etherscan.io/tx`;
 export const BEACONSCAN_URL             = IS_MAINNET ? 'https://beaconscan.com/validator' : `https://beaconscan.com/${NETWORK_NAME.toLowerCase()}/validator`;
